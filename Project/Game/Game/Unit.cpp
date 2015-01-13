e289d35d45efc05e802ff892d02a1868ea88eff7
Keith Adler <Talador12@gmail.com>
2015-01-12 18:06:20 -0600

    movement looks better, but still not right (you'll see)

--- Project/Game/Game/Unit.cpp
+++ Project/Game/Game/Unit.cpp
@@ -25,13 +25,12 @@ public:
 		name = "Unit Name";
 		desc = "Unit Desc";
 		HP = 10;
-		movement = 10;
+		movement = 1;
 		atk = 1;
 		def = 1;
 		mgAtk = 1;
 		mgDef = 1;
 		moving = false;
-		cout << "harhar";
 	}
 
 	//Arrival check
@@ -47,32 +46,24 @@ public:
 		return false;
 	}
 	void move(int display_width, int display_height)
-	{
-		//This part is breaking "Click to move"
-		int distanceX = (sgn(dest.X - loc.X) * (abs(dest.X) - abs(loc.X)));
-		int distanceY = (sgn(dest.Y - loc.Y) * (abs(dest.Y) - abs(loc.Y)));
-		
-		if (distanceX != 0)
+	{	
+		float distance = getDistance(loc.X, loc.Y, dest.X, dest.Y);
+
+		if (distance != 0)
 		{
-			loc.X += (sgn(dest.X - loc.X)*5 + distanceX) / (1000 * movement);
+			if (loc.X != dest.X)
+			{
+				loc.X = (loc.X - ((loc.X - dest.X) / distance) * movement);
+			}
+
+			if (loc.Y != dest.Y)
+			{
+				loc.Y = (loc.Y - ((loc.X - dest.Y) / distance) * movement);
+			}
 		}
 		else
 		{
-			loc.X = dest.X;
-		}
-		if (distanceY != 0)
-		{
-			loc.Y += (sgn(dest.Y - loc.Y)*5 + distanceY) / (1000 * movement);
-		}
-		else
-		{
-			loc.Y = dest.Y;
-		}
-
-		//If arrived at destination
-		if (arrived())
-		{
-
+			arrived();
 		}
 	}
 private:
@@ -81,5 +72,9 @@ private:
 	template <typename T> int sgn(T val)
 	{
 		return (T(0) < val) - (val < T(0));
+	}
+	double getDistance(double dX0, double dY0, double dX1, double dY1)
+	{
+		return sqrt((dX1 - dX0)*(dX1 - dX0) + (dY1 - dY0)*(dY1 - dY0));
 	}
 };
